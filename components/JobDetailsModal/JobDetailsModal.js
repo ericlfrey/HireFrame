24f1ad4cdc28787650ddf439d5852fd856ee7338
Eric Frey <ericlfrey@gmail.com>
2023-07-21 15:09:25 -0500

    Added a Job Details Modal component.

---
+++ components/JobDetailsModal/JobDetailsModal.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+export default function JobDetailsModal({ categoryName }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        +
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{categoryName}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+JobDetailsModal.propTypes = {
+  categoryName: PropTypes.string.isRequired,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import { Button, ListGroup } from 'react-bootstrap';
+import { ListGroup } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import { useAuth } from '../utils/context/authContext';
 import { getAllFilteredJobs } from '../utils/data/jobData';
 import JobCard from '../components/JobCard/JobCard';
+import JobDetailsModal from '../components/JobDetailsModal/JobDetailsModal';
 
 function Home() {
   const [filteredJobs, setFilteredJobs] = useState([]);
@@ -19,19 +20,14 @@ function Home() {
     getAllFilteredJobs().then(setFilteredJobs);
   }, []);
 
-  const addJob = (e) => {
-    const [, category] = e.target.id.split('--');
-    console.log(category);
-  };
   return (
     <>
       <div className="main-page-container">
-        <h1>Hello {user.displayName}! Welcome to HireFrame!</h1>
         <ListGroup key="sm" horizontal="sm" className="my-2">
           {categories.map((cat) => (
             <ListGroup.Item style={{ width: '19%' }} key={cat.name}>
               <h6>{cat.name}</h6>
-              <Button onClick={addJob} id={`btn--${cat.name}`}>+</Button>
+              <JobDetailsModal categoryName={cat.name} />
               {cat.array?.map((job) => (
                 <JobCard
                   key={job.id}
