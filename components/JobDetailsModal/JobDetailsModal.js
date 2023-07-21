b35931f91412c71ec4b0f68c83011ca0b26d1b3e
Eric Frey <ericlfrey@gmail.com>
2023-07-21 15:57:58 -0500

    Made a basic format for the modal details/form.

--- components/JobDetailsModal/JobDetailsModal.js
+++ components/JobDetailsModal/JobDetailsModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 export default function JobDetailsModal({ categoryName }) {
@@ -15,15 +15,36 @@ export default function JobDetailsModal({ categoryName }) {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Add Job</Modal.Title>
         </Modal.Header>
-        <Modal.Body>{categoryName}</Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Company</Form.Label>
+              <Form.Control type="text" placeholder="Company" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Job Title</Form.Label>
+              <Form.Control type="text" placeholder="Job Title" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>List</Form.Label>
+              <Form.Select>
+                <option>Open this select menu</option>
+                <option value="1">{categoryName}</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </Form.Select>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            Close
+            Discard
           </Button>
           <Button variant="primary" onClick={handleClose}>
-            Save Changes
+            Save Job
           </Button>
         </Modal.Footer>
       </Modal>
