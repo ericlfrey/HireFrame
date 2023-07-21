83ebf005b16a82c9b44fe14c201064d3e66f56e0
Eric Frey <ericlfrey@gmail.com>
2023-07-21 16:30:01 -0500

    Added some functionality to the add job form. The text inputs are set to required, but it is not showing an asterisk when it is empty.

--- components/AddJobModal/AddJobModal.js
+++ components/AddJobModal/AddJobModal.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 export default function AddJobModal({ categoryName }) {
   const [show, setShow] = useState(false);
+  const [formInput, setFormInput] = useState({});
+
+  const categories = ['Wishlist', 'Applied', 'Offer', 'Interview', 'Rejected'];
+
+  useEffect(() => {
+    setFormInput({
+      company: '',
+      jobTitle: '',
+      category: categoryName,
+    });
+  }, [categoryName]);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formInput);
+  };
+
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
@@ -21,32 +46,49 @@ export default function AddJobModal({ categoryName }) {
           <Form>
             <Form.Group className="mb-3">
               <Form.Label>Company</Form.Label>
-              <Form.Control type="text" placeholder="Company" />
+              <Form.Control
+                type="text"
+                placeholder="Company"
+                name="company"
+                value={formInput.company}
+                onChange={handleChange}
+                autoComplete="off"
+                required
+              />
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Job Title</Form.Label>
-              <Form.Control type="text" placeholder="Job Title" />
+              <Form.Control
+                type="text"
+                placeholder="Job Title"
+                name="jobTitle"
+                value={formInput.jobTitle}
+                onChange={handleChange}
+                autoComplete="off"
+                required
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>List</Form.Label>
-              <Form.Select>
-                <option>Open this select menu</option>
-                <option value="1">{categoryName}</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+              <Form.Select
+                onChange={handleChange}
+                name="category"
+                value={formInput.category}
+              >
+                {categories.map((cat) => (
+                  <option key={cat}>{cat}</option>
+                ))}
               </Form.Select>
             </Form.Group>
+            {/* <Button type="button" variant="secondary" onClick={handleClose}>
+              Discard
+            </Button> */}
+            <Button type="submit" variant="primary" onClick={handleSubmit}>
+              Save Job
+            </Button>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Discard
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Job
-          </Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
