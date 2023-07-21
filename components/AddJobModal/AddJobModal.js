070571960a2a4ee55b354341b389ce3d4777271f
Eric Frey <ericlfrey@gmail.com>
2023-07-21 17:47:18 -0500

    The add job form is creating a payload. The next step will be to create an API call that creates a new object.

--- components/AddJobModal/AddJobModal.js
+++ components/AddJobModal/AddJobModal.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { useAuth } from '../../utils/context/authContext';
 
 export default function AddJobModal({ categoryName }) {
   const [show, setShow] = useState(false);
   const [formInput, setFormInput] = useState({});
+
+  const { user } = useAuth();
 
   const categories = ['Wishlist', 'Applied', 'Offer', 'Interview', 'Rejected'];
 
@@ -15,6 +18,18 @@ export default function AddJobModal({ categoryName }) {
       category: categoryName,
     });
   }, [categoryName]);
+
+  const getJobStatus = (status) => {
+    const statusMap = {
+      Wishlist: 1,
+      Applied: 2,
+      Offer: 3,
+      Interview: 4,
+      Rejected: 5,
+    };
+
+    return statusMap[status];
+  };
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -29,7 +44,17 @@ export default function AddJobModal({ categoryName }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formInput);
+    if (formInput.company === '' || formInput.jobTitle === '') window.alert('All Fields must be valid');
+    const payload = {
+      company: formInput.company,
+      title: formInput.jobTitle,
+      dateCreated: new Date().toLocaleDateString(),
+      description: '',
+      status: getJobStatus(formInput.category),
+      userId: user.uid,
+    };
+    console.log(payload);
+    handleClose();
   };
 
   return (
@@ -53,7 +78,7 @@ export default function AddJobModal({ categoryName }) {
                 value={formInput.company}
                 onChange={handleChange}
                 autoComplete="off"
-                required
+                required="required"
               />
             </Form.Group>
 
