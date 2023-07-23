0b6b5a3b465f08a428a9537d3b22077aacfd900e
Eric Frey <ericlfrey@gmail.com>
2023-07-23 09:26:46 -0500

    WIP. Added the modal to the JobCard component and it works now. I will need to figure out how to separate the components.

--- components/JobCard/JobCard.js
+++ components/JobCard/JobCard.js
@@ -1,15 +1,22 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Modal } from 'react-bootstrap';
 import { deleteJob } from '../../utils/data/jobData';
 
 export default function JobCard({ job, refreshPage }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const handleDelete = () => {
+    setShow(false);
     deleteJob(job.id).then(() => refreshPage());
   };
   return (
-    <Card style={{ width: '90%' }}>
-      <Card.Body>
+    <>
+      <Card style={{ width: '90%' }}>
         <Button
           variant="outline-danger"
           onClick={handleDelete}
@@ -18,12 +25,32 @@ export default function JobCard({ job, refreshPage }) {
           }}
         >X
         </Button>
-        <Card.Title style={{ fontSize: '1rem' }}>{job.company}</Card.Title>
-        <Card.Text style={{ fontSize: '0.7rem' }}>
-          {job.title}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+        <a href="#" onClick={handleShow}>
+          <Card.Body>
+
+            <Card.Title style={{ fontSize: '0.7rem', fontWeight: 'bold' }}>{job.company}</Card.Title>
+            <Card.Text style={{ fontSize: '0.7rem' }}>
+              {job.title}
+            </Card.Text>
+          </Card.Body>
+        </a>
+      </Card>
+
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
