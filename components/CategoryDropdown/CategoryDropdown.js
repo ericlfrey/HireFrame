5e6aa6ac72a1b864893e94cee8ec4c7336c3dd84
Eric Frey <ericlfrey@gmail.com>
2023-07-24 10:00:04 -0500

    I'm going to try to use Context API to send a function. I havent done this before

--- components/CategoryDropdown/CategoryDropdown.js
+++ components/CategoryDropdown/CategoryDropdown.js
@@ -1,27 +1,46 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import { getJobById } from '../../utils/data/jobData';
+import { useRouter } from 'next/router';
+import { getAllFilteredJobs, getJobById, patchJob } from '../../utils/data/jobData';
 
 export default function CategoryDropdown({ jobId }) {
-  const [job, setJob] = useState();
+  const [, setJob] = useState();
+  const [, setFilteredJobs] = useState([]);
+  const [currentStatus, setCurrentStatus] = useState('');
+
+  const router = useRouter();
+
+  const getFilteredJobs = () => getAllFilteredJobs().then(setFilteredJobs);
 
   useEffect(() => {
-    getJobById(jobId).then(setJob);
-    console.log(job);
+    getJobById(jobId).then((jobData) => {
+      setJob(jobData);
+      setCurrentStatus(jobData?.status || ''); // Set status to empty string if jobData or status is not available
+    });
+    getFilteredJobs();
   }, [jobId]);
+
+  const handleChange = (status) => {
+    setCurrentStatus(status);
+    patchJob({ id: jobId, status }).then(() => {
+      getFilteredJobs().then(() => router.push('/'));
+    });
+  };
 
   const categories = ['Wishlist', 'Applied', 'Offer', 'Interview', 'Rejected'];
 
   return (
     <DropdownButton drop="down-centered" title="Move" size="sm">
-      {categories.map((cat) => (
+      {categories.map((category) => (
         <Dropdown.Item
           href="#/action-1"
-          key={cat}
-          name={cat}
+          key={category}
+          disabled={currentStatus === category} // Disable the item if it matches the current status
+          onClick={() => handleChange(category)} // Handle the click event to update the status
         >
-          {cat}
+          {category}
         </Dropdown.Item>
       ))}
     </DropdownButton>
