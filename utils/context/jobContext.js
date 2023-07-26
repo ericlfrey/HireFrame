92185c1b1de6f85cabd9bc5676df141321c4aefc
Eric Frey <ericlfrey@gmail.com>
2023-07-26 15:05:11 -0500

    Changed the API call, and the use context function to make sure only the logged in user sees their own data.

--- utils/context/jobContext.js
+++ utils/context/jobContext.js
@@ -4,13 +4,15 @@ import React, {
   createContext, useState, useContext, useEffect,
 } from 'react';
 import { getAllFilteredJobs } from '../data/jobData';
+import { useAuth } from './authContext';
 
 const JobContext = createContext();
 
 export function JobProvider({ children }) {
   const [filteredJobs, setFilteredJobs] = useState([]);
+  const { user } = useAuth();
 
-  const getFilteredJobs = () => getAllFilteredJobs().then(setFilteredJobs);
+  const getFilteredJobs = () => getAllFilteredJobs(user.uid).then(setFilteredJobs);
 
   useEffect(() => {
     getFilteredJobs();
