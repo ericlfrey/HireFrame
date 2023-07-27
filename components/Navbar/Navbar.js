2e39f08c369e18072778792539bd64f0b8b7ad0c
Eric Frey <ericlfrey@gmail.com>
2023-07-27 17:38:49 -0500

    I imported the correct Navbar to the ViewDirector file.

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import {
 import Link from 'next/link';
 import MenuDrawer from '../MenuDrawer/MenuDrawer';
 
-export default function NavbarOffCanvas() {
+export default function NavBar() {
   return (
     <Navbar key={false} expand={false} style={{ backgroundColor: '#3E3E3C' }}>
       <Container fluid>
