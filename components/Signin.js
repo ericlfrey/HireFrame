77cdba268af301a1f4e780b2826826214bebaf0f
Eric Frey <ericlfrey@gmail.com>
2023-07-31 17:12:11 -0500

    Ready to push.

--- components/Signin.js
+++ components/Signin.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import Image from 'next/image';
+import Head from 'next/head';
+import { Button } from 'react-bootstrap';
 import { signIn } from '../utils/auth';
 import logo from '../public/images/logo.png';
 
 function Signin() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
-      <button type="button" className="btn btn-primary btn-lg copy-btn" onClick={signIn}>
-        Sign In
-      </button>
-      <Image src={logo} />
-    </div>
+    <>
+      <Head>
+        <title>HireFrame</title>
+      </Head>
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          maxWidth: '400px',
+          margin: 'auto',
+        }}
+      >
+        <button type="button" style={{ all: 'unset' }} onClick={signIn}>
+          <h1 style={{ color: '#EEF0F2', fontSize: '3.5rem', cursor: 'pointer' }}>HireFrame</h1>
+        </button>
+        <button type="button" style={{ all: 'unset', cursor: 'pointer' }} onClick={signIn}>
+          <Image
+            onClick={signIn}
+            src={logo}
+            className="logo"
+            priority
+          />
+        </button>
+        <Button
+          type="button"
+          className="btn-style"
+          onClick={signIn}
+          style={{ border: 'solid 1px #EEF0F2' }}
+        >
+          Sign In With Google
+        </Button>
+      </div>
+    </>
   );
 }
 
